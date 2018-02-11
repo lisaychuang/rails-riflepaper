d23edfc4a47a923fb05885af3a338753f99c0eaf
Lisa Huang <lisa.yc.huang@gmail.com>
2018-02-10 16:36:25 -0800

    Generated User model and controller. Added Bcrypt to gemfile, and has_secure_password to user mode. Created user signup form in views #new and #create action

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ require 'nokogiri'
 require 'open-uri'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :user
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
