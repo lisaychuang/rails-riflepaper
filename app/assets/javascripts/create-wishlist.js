e403998ba2ff55607feb5fc02b7c7b75dad709f9
Lisa Huang <lisa.yc.huang@gmail.com>
2018-09-08 16:21:45 -0700

    Refactor JS for wishlist creation, update form partial and show page

---
+++ app/assets/javascripts/create-wishlist.js
@@ -0,0 +1,46 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+function createWishlist(url, data) {
+  $.post(url, data, "json")
+    // on success create a new instance of our model object
+    .done(json => {
+      let myWishList = new Wishlist(json);
+      myWishList.updateHTML();
+    })
+    .error(resp => {
+      alert("There is an error!", resp);
+    });
+}
+
+// Wishlist class constructor
+// {id: 41, name: "qwdq", notes: "wq", budget: 0, products: []}
+class Wishlist {
+  constructor(json) {
+    this.data = json;
+  }
+
+  // wishlist class instance method
+  updateHTML() {
+    $("#wishlist-name").text(this.data.name);
+    $("#wishlist-budget").text(
+      "Budget: $" +
+        this.data.budget.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+    );
+    $("#wishlist-notes").text("Notes: " + this.data.notes);
+  }
+}
+
+function setupCreateWishlist() {
+  $("form").submit(function(event) {
+    debugger;
+    event.preventDefault();
+    // Get values from form
+    let values = $(this).serialize();
+    // url from form's action=?
+    let url = $(this).attr("action");
+
+    // perform AJAX call
+    createWishlist(url, values);
+  });
+}
